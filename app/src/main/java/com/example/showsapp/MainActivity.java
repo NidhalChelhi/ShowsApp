0431e78c1e23de12a00e17be93c4e5f1b3dd9fb4
NidhalChelhi <nidhalelchelhi@gmail.com>
2025-04-14 00:06:56 +0100

    Enhance App

--- app/src/main/java/com/example/showsapp/MainActivity.java
+++ app/src/main/java/com/example/showsapp/MainActivity.java
@@ -4,10 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import androidx.appcompat.widget.SearchView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -29,10 +29,8 @@ public class MainActivity extends AppCompatActivity implements ShowsAdapter.OnSh
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize toolbar
         MaterialToolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitle("Upcoming Shows");
 
         // Initialize RecyclerView
         showsRecyclerView = findViewById(R.id.showsRecyclerView);
